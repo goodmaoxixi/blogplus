b005d8dbb7a641d948890cd7d00c3f54d78555ae
GuanglinDu <guanglin.du@gmail.com>
2014-11-01 11:07:42 +0800

    Add comment editing support and enhanced the welcome#index page.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,66 @@
+class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :show, :edit, :update, :destroy]
+  before_action :set_comment, :only => [:show, :edit, :update, :destroy]
+
+  # Authentication
+  http_basic_authenticate_with name: "test",  password: "test", only: :destroy
+
+  # POST /posts/:post_id/comments
+  # POST /posts/:post_id/comments.xml
+  def create
+    @comment = @post.comments.create(comment_params)
+    redirect_to post_path(@post)
+  end
+
+  # Notice: Updates are implemented by methods edit and update
+  def edit
+  end 
+ 
+  # PUT /posts/:post_id/comments/:id
+  # PUT /posts/:post_id/comments/:id.xml
+  def update
+    respond_to do |format|
+      if @comment.update(comment_params)
+        #1st argument of redirect_to is an array, in order to build the correct route to the nested resource comment
+        # Stay at the comment editing page
+        #format.html { redirect_to [@comment.post, @comment], notice: 'Comment was successfully updated.' }
+        # To the comment list page
+        format.html { redirect_to @comment.post, notice: 'Comment was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def show
+  end
+  
+  # Redirection to the list of Comments: redirect_to(post_comments_url)
+  # See: http://blog.8thcolor.com/en/2011/08/nested-resources-with-independent-views-in-ruby-on-rails/
+  #1st you retrieve the post thanks to params[:post_id]
+  # post = Post.find(params[:post_id])
+  # 2nd you retrieve the comment thanks to params[:id]
+  # @comment = post.comments.find(params[:id])
+  def destroy
+      @comment.destroy
+      redirect_to post_path(@post) # original version
+      #redirect_to @comment.post # also works
+  end
+  
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:post_id])
+    end
+
+    def set_comment
+      @comment = Comment.find(params[:id])
+    end
+  
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def comment_params
+      params[:comment].permit(:commenter, :body)
+    end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,65 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
+  # Authentication
+  http_basic_authenticate_with name: "test", password: "test", except: [:index, :show]
+
+  def new
+    @post = Post.new
+  end
+    
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    #render text: params[:post].inspect
+    #@post = Post.new(params[:post])
+    @post = Post.new(post_params)
+    # Not working in 4.0.3. DGL 20140917
+    #@post = Post.new(params[:post].permit(:titile, :text))  
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  # Updates are implemented by methods edit and update
+  def edit
+    #@post = Post.find(params[:id]) # replaced by set_post
+  end
+
+  def update
+    #@post = Post.find(params[:id]) # replaced by set_post
+    #if @post.update(params[:post].permit(:title, :text))
+    if @post.update(post_params)    
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    #@post = Post.find(params[:id]) # replaced by set_post
+  end
+
+  # Delete a record
+  def destroy
+    #@post = Post.find(params[:id]) # replaced by set_post
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+end
